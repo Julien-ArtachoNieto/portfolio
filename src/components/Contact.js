51082f22e647aaf179f4d12fe84b26a6bc0d5398
Julien-ArtachoNieto <julien.artachonieto@gmail.com>
2022-06-01 09:36:41 +0200

    Ajout reinitialisation formulaire

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -7,6 +7,11 @@ const Contact =()=>{
   const publickey=process.env.REACT_APP_EMAILJS_KEY
   const templateid=process.env.REACT_APP_TEMPLATE_ID
   const serviceid=process.env.REACT_APP_SERVICE_ID
+  const [contactName,setContactName]=useState('')
+  const [contactEntreprise,setContactEntreprise]=useState('')
+  const [contactMail,setContactMail]=useState('')
+  const [contactMsg,setContactMsg]=useState('')
+
   const [toggle,setToggle]=useState(false)
   const modalToggle =()=>{
     const modalWindow=document.querySelector(".modal-bg")
@@ -20,6 +25,7 @@ const Contact =()=>{
   }
   const sendEmail = (e) => {
     e.preventDefault();
+
     emailjs.sendForm(serviceid,templateid, form.current, publickey)
       .then((result) => {
         console.log(result.text);
@@ -27,6 +33,10 @@ const Contact =()=>{
         console.log(error.text);
       });
     modalToggle()
+    setContactName('')
+    setContactEntreprise('')
+    setContactMail('')
+    setContactMsg('')
   };
 
   return (
@@ -34,13 +44,13 @@ const Contact =()=>{
       <p className="contact-speech">L'endroit parfait pour me contacter, me faire une proposition sérieuse ou bien un retour constructif. </p>
       <form className="contact-form-container" ref={form} onSubmit={sendEmail}>
         <label>Nom</label>
-        <input type="text" name="user_name" required/>
+        <input type="text" name="user_name" value={contactName} onChange={(event)=>setContactName(event.target.value)} required/>
         <label>Entreprise</label>
-        <input type="text" name="user_entreprise" required/>
+        <input type="text" name="user_entreprise"  value={contactEntreprise} onChange={(event)=>setContactEntreprise(event.target.value)} required/>
         <label>Email</label>
-        <input type="email" name="user_email" required/>
+        <input type="email" name="user_email" value={contactMail} onChange={(event)=>setContactMail(event.target.value)} required/>
         <label>Message</label>
-        <textarea className="contact-msg" name="message" required/>
+        <textarea className="contact-msg" name="message" value={contactMsg} onChange={(event)=>setContactMsg(event.target.value)} required/>
         <button className="contact-form-submit-btn" type="submit" >Envoyer</button>
       </form>
       <div className="modal-bg">
